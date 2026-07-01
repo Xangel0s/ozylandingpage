766390df109f8e6724b0048d84a7656a529c599b
Xangel0s <zastuto5@gmail.com>
2026-07-01 09:51:39 -0500

    feat: integrate Google Tag Manager (head script and noscript body)

--- ozybase-landing/src/app/layout.tsx
+++ ozybase-landing/src/app/layout.tsx
@@ -83,6 +83,15 @@ export default function RootLayout({
       style={{ colorScheme: "dark" }}
       suppressHydrationWarning
     >
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TSVD59VV');
+        `}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-18290656203"
         strategy="afterInteractive"
@@ -107,6 +116,15 @@ export default function RootLayout({
         className="min-h-full flex flex-col bg-background text-foreground selection:bg-success-neon selection:text-black"
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSVD59VV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ScrollRevealManager />
         {children}
         <WhatsAppFloating />
